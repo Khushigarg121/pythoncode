abaa2b6c1a10d9d7021150467d4b7ca97837c3bc
khushigarg121 <khushigarg813@gmail.com>
2019-05-18 09:49:15 +0530

    better one

--- addition.py
+++ addition.py
@@ -1,3 +1,8 @@
 x=int(input("enter value of x"))
 y=int(input("enter value of y"))
-print("x+y=",x+y)
+print(f"x={x}")
+print(f"y={y}")
+print(f"{x}+{y} = {x+y}".center(60))
+printf("thanks for this progrram".center(60))
+
+
